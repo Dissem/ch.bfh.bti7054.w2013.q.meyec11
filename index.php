6f8d49d39b26cdb019421a45ce69c8068029eb02
Christian Meyer <chrigu.meyer@gmail.com>
2013-10-22 20:09:56 +0200

    Carousel is not PHP-rendered, too

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <?php
   include('views/navbar.php');
+  include('views/carousel.php');
 ?>
   <head>
     <title>Chris' CustomArt Webshop</title>
@@ -28,81 +29,35 @@
 //  $me->addSubEntry(new MenuEntry("Test 4"));
 //  $navBar->addSubEntry($me);
   $navBar->render();
+
+  $carousel = new Carousel("artCarousel");
+  $slide = new Slide();
+  $slide->title = "Art, as you've never experienced before.";
+  $slide->subtitle = "Except if you did, then you might have.";
+  $slide->text = "You can choose the style, subject and author of the picture.";
+  $slide->image = "dragon-artist.png";
+  $carousel->addSlide($slide);
+  $slide = new Slide();
+  $slide->title = "Each piece an unicate.";
+  $slide->subtitle = "Or even a duplicate.";
+  $slide->text = "It's generated especially for you, with much love, by our servers.";
+  $slide->image = "dragon-artist.png";
+  $carousel->addSlide($slide);
+  $slide = new Slide();
+  $slide->title = "Blazing fast.";
+  $slide->subtitle = "On a blazing fast internet connection.";
+  $slide->text = "After you paid it, you can download it directly.";
+  $slide->image = "dragon-artist.png";
+  $carousel->addSlide($slide);
+  $slide = new Slide();
+  $slide->title = "Pay with Bitcoin.";
+  $slide->subtitle = "The internet money you haven't heard of. Yet.";
+  $slide->text = "If you already know Bitcoin, you can always try to make me accept Litecoin as well.";
+  $slide->image = "dragon-bitcoin.png";
+  $carousel->addSlide($slide);
+  $carousel->render();
   ?>
 
-    <div id="artCarousel" class="carousel slide">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#artCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#artCarousel" data-slide-to="1"></li>
-        <li data-target="#artCarousel" data-slide-to="2"></li>
-        <li data-target="#artCarousel" data-slide-to="3"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="img/dragon-artist.png" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Art, as you've never experienced before. <span class="text-muted">Except
-                  if you did, then you might have.</span></h1>
-              <p>
-                You can choose the style, subject and author of the
-                picture.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="img/dragon-artist.png" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Each piece an unicate. <span class="text-muted">Or even a duplicate.</span></h1>
-              <p>
-                It's generated especially for you, with much love, by
-                our servers.
-              </p>
-              <a class="btn btn-large btn-primary" href="#">Learn more</a>
-            </div>
-          </div>
-        </div>
-        <!-- item -->
-        <div class="item">
-          <img src="img/dragon-artist.png" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Blazing fast. <span class="text-muted">On a blazing fast internet
-                  connection.</span></h1>
-              <p class="lead">
-                After you paid it, you can download it directly.
-              </p>
-            </div>
-          </div>
-        </div>
-        <!-- item -->
-        <div class="item">
-          <img src="img/dragon-bitcoin.png" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Pay with Bitcoin. <span class="text-muted">The internet money you haven't
-                  heard of. Yet.</span></h1>
-              <p class="lead">
-                If you already know Bitcoin, you can always try to make
-                me accept Litecoin as well.
-              </p>
-            </div>
-          </div>
-          <!-- container -->
-        </div>
-        <!-- item -->
-      </div>
-      <!-- carousel-inner -->
-      <a class="left carousel-control" href="#artCarousel" data-slide="prev">&lsaquo;</a>
-      <a class="right carousel-control" href="#artCarousel" data-slide="next">&rsaquo;</a>
-    </div>
-    <!-- carousel -->
-    <?php
-
-    ?>
     <script src="js/jquery.min.js">
     </script>
     <script src="js/bootstrap.min.js">
--- views/carousel.php
+++ views/carousel.php
@@ -1,6 +1,42 @@
 <?php
 class Carousel {
-  private $slides = array();
+  public $id;
+  private $slides;
+
+  public function __construct($id){
+    $this->id = $id;
+  }
+
+  public function render() {
+    ?>
+    <div id="<?php echo $this->id; ?>" class="carousel slide">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#<?php echo $this->id; ?>" data-slide-to="0" class="active"></li>
+      <?php
+        for ($i = 1; $i < count($this->slides); $i++) {
+          echo "        <li data-target=\"#$this->id\" data-slide-to=\"$i\"></li>\n";
+        }
+      ?>
+      </ol>
+      <div class="carousel-inner">
+      <?php
+        foreach ($this->slides as $slide) {
+          if ($slide == $this->slides[0])
+            $slide->active = true;
+          $slide->render();
+        }
+      ?>
+      </div>
+      <a class="left carousel-control" href="#<?php echo $this->id; ?>" data-slide="prev">&lsaquo;</a>
+      <a class="right carousel-control" href="#<?php echo $this->id; ?>" data-slide="next">&rsaquo;</a>
+    </div>
+    <?php
+  }
+
+  public function addSlide(Slide $slide) {
+    $this->slides[] = $slide;
+  }
 }
 
 class Slide {
@@ -8,4 +44,19 @@ class Slide {
   public $title;
   public $subtitle;
   public $text;
+  public $active = false;
+
+  public function render() {
+    ?>
+        <div class="item<?php if ($this->active) echo " active"; ?>">
+          <img src="img/<?php echo $this->image; ?>" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1><?php echo $this->title; ?> <span class="text-muted"><?php echo $this->subtitle; ?></span></h1>
+              <p><?php echo $this->text; ?></p>
+            </div>
+          </div>
+        </div>
+    <?php
+  }
 }
